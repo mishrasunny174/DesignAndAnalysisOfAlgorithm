19c863af17679c8721f4e65c49c6846d13ce5511
mishrasunny174 <mishrasunny174@gmail.com>
2019-03-22 21:33:29 +0530

    added merge sort

---
+++ sorting/MergeSort.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+
+using namespace std;
+
+inline void printArray(int *array, int size) {
+    cout<<"{ ";
+    for(int i=0; i<size; i++ ) 
+        cout<<array[i]<<' ';
+    cout<<"}"<<endl;
+}
+
+void merge(int* array, int start, int mid, int end) {
+    int size = end - start + 1;
+    int *tempArray = new int[size];
+    int counter1=start, counter2=mid+1, counter3=0;
+    while(counter1<=mid && counter2<=end) {
+        if(array[counter1] < array[counter2]) {
+            tempArray[counter3++] = array[counter1++];
+        } else {
+            tempArray[counter3++] = array[counter2++];
+        }
+    }
+    while(counter1<=mid) {
+        tempArray[counter3++] = array[counter1++];
+    }
+    while(counter2<=end) {
+        tempArray[counter3++] = array[counter2++];
+    }
+    for(int i=0; i<counter3; i++) {
+        array[i+start] = tempArray[i];
+    }
+}
+
+void mergeSort(int* array,int start, int end) {
+    if (start < end) {
+        int mid = (start+end)/2;
+        mergeSort(array, start, mid);
+        mergeSort(array, mid+1, end);
+        merge(array, start, mid, end);
+    }
+}
+
+int main() {
+    int size;
+    int *array;
+    cout<<"Enter size of array: ";
+    cin>>size;
+    array = new int[size];
+    cout<<"Enter array elements separated by space: ";
+    for(int i=0; i<size; i++) {
+        cin>>array[i];
+    }
+    mergeSort(array, 0, size-1);
+    printArray(array, size);
+    return 0;
+}
